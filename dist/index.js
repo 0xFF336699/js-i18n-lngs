ce1ce052f341cb1d8920610eb23d5ba9fe29a4b3
0xFF336699 <0xFF336699@gmail.com>
2024-09-12 18:58:47 +0800

    1.0.13

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,7 @@
-export * from "./gemini-supports";
-export * from "./gemini-supports-en";
-export * from "./lngs-arr";
-export * from "./lngs-chrome-locale";
-export * from "./lngs-chrome";
-export * from "./lngs-chrome-zh-CN";
-export * from "./lngs-en";
-export * from "./lngs-zhCN";
-export * from "./lng-locale";
-export * from "./type";
-//# sourceMappingURL=index.js.map
+// src/index.ts
+function myPackage(taco = "") {
+  return `${taco} from my package`;
+}
+export {
+  myPackage
+};
